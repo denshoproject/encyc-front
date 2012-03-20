2d8fd60761772532f0cf0ec7ae30891bff27b6b2
geoffrey jost <geoffrey.jost@densho.org>
2012-03-19 18:51:43 -0700

    redirect from / to /mediawiki/index.php/Main_Page

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.http import HttpResponseRedirect
 
 #from django.contrib import admin
 #admin.autodiscover()
@@ -7,4 +8,5 @@ urlpatterns = patterns(
     '',
     url(r'^mediawiki/', include('wikiprox.urls')),
     #url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', lambda x: HttpResponseRedirect('/mediawiki/index.php/Main_Page')),
 )

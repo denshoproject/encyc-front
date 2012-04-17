01c0e17163940be981b30b46a3ca0e9a8f879c56
geoffrey jost <gjost@linux.com>
2012-04-17 13:17:45 -0700

    unit test for problematic chars in wiki page titles
    Make sure Front can work with titles containing commas, parens,
    slashes, single-quotes, etc.

--- apps/wikiprox/tests.py
+++ apps/wikiprox/tests.py
@@ -1,16 +1,30 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+MW_URL = '/mediawiki/index.php/%s/'
+
+def get_response(url):
+    c = Client()
+    return c.get(url)
+def get_status(url):
+    return get_response(url).status_code
+
+
+class WikiPageTitles(TestCase):
+    """Test that characters in MediaWiki titles are matched correctly
+    """
+    def test_wiki_titles_space(self):
+        self.assertEqual(200, get_status(MW_URL % 'Ansel Adams'))
+    def test_wiki_titles_period(self):
+        self.assertEqual(200, get_status(MW_URL % 'A.L. Wirin'))
+    def test_wiki_titles_hyphen(self):
+        self.assertEqual(200, get_status(MW_URL % 'Aiko Herzig-Yoshinaga'))
+    def test_wiki_titles_parens(self):
+        self.assertEqual(200, get_status(MW_URL % 'Amache (Granada)'))
+    def twiki_titleshars_comma(self):
+        self.assertEqual(200, get_status(MW_URL % 'December 7, 1941'))
+    def test_wiki_titles_singlequote(self):
+        self.assertEqual(200, get_status(MW_URL % "Hawai'i"))
+    def test_wiki_titles_slash(self):
+        self.assertEqual(200, get_status(MW_URL % 'Informants / inu'))

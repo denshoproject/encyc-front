5630274bba9760f3837e3328bd826ebd2161ae6e
geoffrey jost <gjost@linux.com>
2012-04-17 11:20:02 -0700

    match MediaWiki page titles with problematic chars
    periods, commas, parentheses, slashes, single-quotes, etc.

--- apps/wikiprox/urls.py
+++ apps/wikiprox/urls.py
@@ -5,11 +5,21 @@ from wikiprox import views
 urlpatterns = patterns(
     '',
     # files (tansu)
-    url(r'^index.php/File:([\w .:_-]+)/$', views.media, name='wikiprox-media'),
-    url(r'^File:([\w .:_-]+)/$', views.media, name='wikiprox-media'),
+    url(r"^index.php/File:([\w .:_-]+)/$", views.media, name='wikiprox-media'),
+    url(r"^File:([\w .:_-]+)/$", views.media, name='wikiprox-media'),
     # pages (mediawiki)
-    url(r'^index.php/([\w .:_-]+)/$', views.page, name='wikiprox-page'),
-    url(r'^([\w .:_-]+)/$', views.page, name='wikiprox-page'),
+    url(r"^index.php/([()/'\w .,:_-]+)/$", views.page, name='wikiprox-page'),
+    url(r"^([()/'\w .:_-]+)/$", views.page, name='wikiprox-page'),
     #
     url(r'^$', views.index, name='wikiprox-index'),
 )
+
+# problematic page titles
+# period, comma, hyphen, parentheses, slash, single-quote/apostrophe
+# Examples:
+#   A.L. Wirin
+#   Aiko Herzig-Yoshinaga
+#   Amache (Granada)
+#   Bureau of Sociological Research, Poston
+#   Documentary films/videos on incarceration
+#   Hawai'i

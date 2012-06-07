afe8dc9724e69651966901b4bad6788f3cf5835d
geoffrey jost <geoffrey@jostwebwerks.com>
2012-06-07 16:23:55 -0700

    replaced url regex with just-match-everything
    This matches 'Informers_/_"inu"', and maybe some other things
    that it shouldn't...
    refs #126

--- apps/wikiprox/urls.py
+++ apps/wikiprox/urls.py
@@ -8,8 +8,8 @@ urlpatterns = patterns(
     url(r"^index.php/File:([\w .:_-]+)/$", views.media, name='wikiprox-media'),
     url(r"^File:([\w .:_-]+)/$", views.source, name='wikiprox-source'),
     # pages (mediawiki)
-    url(r"^index.php/([()/'\w .,:_-]+)/$", views.page, name='wikiprox-page'),
-    url(r"^([()/'\w .:_-]+)/$", views.page, name='wikiprox-page'),
+    url(r"^index.php/([\w\W]+)/$", views.page, name='wikiprox-page'),
+    url(r"^([\w\W]+)/$", views.page, name='wikiprox-page'),
     #
     url(r'^$', views.index, name='wikiprox-index'),
 )

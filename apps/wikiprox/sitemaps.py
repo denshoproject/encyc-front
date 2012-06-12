d9ee2873d416eadd15acb8ba3dd44f7bfb84a50c
geoffrey jost <geoffrey@jostwebwerks.com>
2012-06-11 17:43:31 -0700

    Google-friendly sitemap.xml
    refs #50

---
+++ apps/wikiprox/sitemaps.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+import json
+
+import requests
+
+from django.contrib.sitemaps import Sitemap
+
+
+class Page(object):
+    location = ''
+    timestamp = None
+    def unicode(self):
+        self.title
+    def get_absolute_url(self):
+        return self.location
+
+
+class MediaWikiSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.5
+
+    def items(self):
+        pages = []
+        #
+        LIMIT = 5000
+        TS_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+        url = 'http://10.0.4.15:9000/mediawiki/api.php?action=query&generator=allpages&prop=revisions&rvprop=timestamp&gaplimit=%s&format=json' % LIMIT
+        r = requests.get(url)
+        if r.status_code == 200:
+            response = json.loads(r.text)
+            if response and response['query'] and response['query']['pages']:
+                ids = []
+                for id  in response['query']['pages'].keys():
+                    if id not in ids:
+                        ids.append(id)
+                for id in ids:
+                    p = response['query']['pages'][id]
+                    page = Page()
+                    page.title = p['title']
+                    page.location = '/wiki/%s/' % p['title']
+                    page.timestamp = datetime.strptime(p['revisions'][0]['timestamp'], TS_FORMAT)
+                    pages.append(page)
+        return pages
+
+    def lastmod(self, obj):
+        return obj.timestamp
--- urls.py
+++ urls.py
@@ -5,9 +5,15 @@ from django.views.generic.simple import direct_to_template
 #from django.contrib import admin
 #admin.autodiscover()
 
+from wikiprox.sitemaps import MediaWikiSitemap
+sitemaps = {
+    'wiki': MediaWikiSitemap,
+}
+
 urlpatterns = patterns(
     '',
     #url(r'^admin/', include(admin.site.urls)),
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
     url(r'^about/$', direct_to_template, {'template': 'about.html'}, name='about'),
     url(r'^search/$', direct_to_template, {'template': 'search.html'}, name='search'),
     url(r'^wiki/', include('wikiprox.urls')),

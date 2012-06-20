0d761df7b04122bbc704ffdc07a5ad9305a4d957
geoffrey jost <geoffrey@jostwebwerks.com>
2012-06-20 12:07:55 -0700

    google custom search id in settings
    refs #49, refs #102

--- context_processors.py
+++ context_processors.py
@@ -13,4 +13,5 @@ def sitewide(request):
         'MEDIA_URL': settings.MEDIA_URL,
         'STATIC_URL': settings.STATIC_URL,
         'WIKIPROX_MEDIAWIKI_DEFAULT_PAGE': settings.WIKIPROX_MEDIAWIKI_DEFAULT_PAGE,
+        'GOOGLE_CUSTOM_SEARCH_PASSWORD': settings.GOOGLE_CUSTOM_SEARCH_PASSWORD,
         }

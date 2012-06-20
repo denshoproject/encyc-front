2e6b910e588ba18ec24eb878a03b5bb14a01c76e
geoffrey jost <geoffrey@jostwebwerks.com>
2012-06-20 14:12:06 -0700

    what_links_here, category_members functions
    category_members function enables lists of Authors, Supplementary
    Materials, etc, etc
    refs #156

--- apps/wikiprox/encyclopedia.py
+++ apps/wikiprox/encyclopedia.py
@@ -25,13 +25,13 @@ def all_pages():
                 pages.append(page)
     return pages
 
-def category_published(namespace_id=None):
-    """Returns list of published pages.
+def category_members(category_name, namespace_id=None):
+    """Returns list of pages with specified Category: tag.
     """
     pages = []
     LIMIT=5000
-    url = '%s?format=json&action=query&generator=categorymembers&gcmtitle=Category:Published' % settings.WIKIPROX_MEDIAWIKI_API
-    if namespace_id:
+    url = '%s?format=json&action=query&generator=categorymembers&gcmtitle=Category:%s' % (settings.WIKIPROX_MEDIAWIKI_API, category_name)
+    if namespace_id != None:
         url = '%s&gcmnamespace=%s' % (url, namespace_id)
     r = requests.get(url, headers={'content-type':'application/json'})
     if r.status_code == 200:
@@ -40,6 +40,13 @@ def category_published(namespace_id=None):
             for id in response['query']['pages']:
                 pages.append(response['query']['pages'][id])
     return pages
+
+def category_article_types():
+    return category_members('Articles')
+def category_authors():
+    return category_members('Authors')
+def category_supplemental():
+    return category_members('Supplemental_Materials')
 
 def namespaces():
     """Returns dict of namespaces and their codes.
@@ -75,9 +82,22 @@ def published_pages():
     """
     pages = []
     pids = []  # published_article_ids
-    for article in category_published(namespace_id=namespaces_reversed()['Default']):
+    for article in category_members('Published', namespace_id=namespaces_reversed()['Default']):
         pids.append(article['pageid'])
     for article in all_pages():
         if article['pageid'] in pids:
             pages.append(article)
     return pages
+
+def what_links_here(title):
+    """Returns titles of pages that link to this one.
+    """
+    titles = []
+    url = '%s?format=json&action=query&list=backlinks&bltitle=%s' % (settings.WIKIPROX_MEDIAWIKI_API, title)
+    r = requests.get(url, headers={'content-type':'application/json'})
+    if r.status_code == 200:
+        response = json.loads(r.text)
+        if response and response['query'] and response['query']['backlinks']:
+            for backlink in response['query']['backlinks']:
+                titles.append(backlink['title'])
+    return titles

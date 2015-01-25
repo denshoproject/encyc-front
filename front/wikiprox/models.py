b56d409a43396600813da01dc7d3b8cdcdc6e7f5
geoffrey jost <geoffrey.jost@densho.org>
2015-01-25 15:34:18 -0800

    cleaned up objects
    - Cleaned up models.  Objects no longer instantiate themselves.
      Instead the Proxy object retrieves data from the MediaWiki/PSMS
      backend and makes objects.
    - Removed cruft from views.

--- front/wikiprox/models.py
+++ front/wikiprox/models.py
@@ -27,33 +27,21 @@ def _columnizer(things, cols):
     columns.append(col)
     return columns
 
-class Wiki(object):
-    """Represents a MediaWiki site
-    
-    NOTE: not a Django model object!
-    """
-    
-    @staticmethod
-    def authors():
-        authors = [page['title'] for page in encyclopedia.published_authors()]
-        return _columnizer(authors, 4)
 
-    @staticmethod
-    def articles_by_category():
-        articles_by_category = []
-        categories,titles_by_category = encyclopedia.articles_by_category()
-        for category in categories:
-            titles = [page['title'] for page in titles_by_category[category]]
-            articles_by_category.append( (category,titles) )
-        return articles_by_category
+class Author(object):
+    url_title = None
+    uri = None
+    title = None
+    title_sort = None
+    body = None
+    lastmod = None
+    author_articles = []
     
-    @staticmethod
-    def contents():
-        articles = [
-            {'first_letter':page['sortkey'][0].upper(), 'title':page['title']}
-            for page in encyclopedia.articles_a_z()
-        ]
-        return articles
+    def __repr__(self):
+        return "<Author '%s'>" % self.title
+    
+    def __str__(self):
+        return self.title
 
 
 class Page(object):
@@ -62,7 +50,7 @@ class Page(object):
     """
     url_title = None
     url = None
-    printed = None
+    uri = None
     status_code = None
     error = None
     public = None
@@ -70,6 +58,7 @@ class Page(object):
     lastmod = None
     is_article = None
     is_author = None
+    title_sort = None
     title = None
     body = None
     sources = []
@@ -78,55 +67,6 @@ class Page(object):
     coordinates = ()
     prev_page = None
     next_page = None
-    
-    def __init__(self, url_title, printed=False, request=None):
-        """
-        @param page: Page title from URL.
-        """
-        logger.debug('%s, printed=%s, request=%s' % (url_title, printed, request))
-        self.url_title = url_title
-        self.printed = printed
-        self.url = mediawiki.page_data_url(settings.WIKIPROX_MEDIAWIKI_API, self.url_title)
-        logger.debug(self.url)
-        auth = (settings.DANGO_HTPASSWD_USER, settings.DANGO_HTPASSWD_PWD)
-        r = requests.get(self.url, auth=auth)
-        self.status_code = r.status_code
-        logger.debug(self.status_code)
-        pagedata = json.loads(r.text)
-        self.error = pagedata.get('error', None)
-        if (self.status_code == 200) and not self.error:
-            self.public = False
-            ## hide unpublished pages on public systems
-            #self.public = request.META.get('HTTP_X_FORWARDED_FOR',False)
-            # note: header is added by Nginx, should not appear when connected directly
-            # to the app server.
-            self.published = mediawiki.page_is_published(pagedata)
-            self.lastmod = mediawiki.page_lastmod(settings.WIKIPROX_MEDIAWIKI_API, self.url_title)
-            # basic page context
-            self.title = pagedata['parse']['displaytitle']
-            self.sources = mediawiki.find_primary_sources(
-                settings.TANSU_API,
-                pagedata['parse']['images'])
-            self.body = mediawiki.parse_mediawiki_text(
-                pagedata['parse']['text']['*'],
-                self.sources,
-                self.public,
-                self.printed)
-            # rewrite media URLs on stage
-            # (external URLs not visible to Chrome on Android when connecting through SonicWall)
-            if hasattr(settings, 'STAGE') and settings.STAGE and request:
-                self.sources = sources.replace_source_urls(self.sources, request)
-            self.is_article = encyclopedia.is_article(self.title)
-            if self.is_article:
-                self.categories = [
-                    c['*'] for c in pagedata['parse']['categories']
-                    if not c.has_key('hidden')]
-                self.prev_page = encyclopedia.article_prev(self.title)
-                self.next_page = encyclopedia.article_next(self.title)
-                self.coordinates = mediawiki.find_databoxcamps_coordinates(pagedata['parse']['text']['*'])
-            self.is_author = encyclopedia.is_author(self.title)
-            if self.is_author:
-                self.author_articles = encyclopedia.author_articles(self.title)
     
     def __repr__(self):
         return "<Page '%s'>" % self.url_title
@@ -137,21 +77,35 @@ class Page(object):
 
 class Source(object):
     encyclopedia_id = None
+    psms_id = None
+    densho_id = None
+    institution_id = None
+    url = None
+    uri = None
+    resource_uri = None
     streaming_url = None
-    rtmp_streamer = ''
-    
-    def __init__(self, encyclopedia_id):
-        self.encyclopedia_id = encyclopedia_id
-        source = sources.source(encyclopedia_id)
-        for key,val in source.iteritems():
-            setattr(self, key, val)
-        self.id = int(source['id'])
-        self.original_size = int(source['original_size'])
-        self.created = datetime.strptime(source['created'], mediawiki.TS_FORMAT)
-        self.modified = datetime.strptime(source['modified'], mediawiki.TS_FORMAT)
-        if getattr(self, 'streaming_url', None) and ('rtmp' in self.streaming_url):
-            self.streaming_url = self.streaming_url.replace(settings.RTMP_STREAMER,'')
-            self.rtmp_streamer = settings.RTMP_STREAMER
+    external_url = None
+    original = None
+    display = None
+    thumbnail_lg = None
+    thumbnail_sm = None
+    media_format = None
+    aspect_ratio = None
+    original_size = None
+    display_size = None
+    title = encyclopedia_id
+    collection_name = None
+    headword = None
+    caption = None
+    caption_extended = None
+    transcript = None
+    courtesy = None
+    creative_commons = None
+    created = None
+    modified = None
+    published = None
+    rtmp_streamer = settings.RTMP_STREAMER
+    authors = {'display':[], 'parsed':[],}
     
     def __repr__(self):
         return "<Source '%s'>" % self.encyclopedia_id
@@ -175,66 +129,135 @@ class Citation(object):
     lastmod = None
     retrieved = None
     authors = []
-    authors_apa = []
-    authors_bibtex = []
-    authors_chicago = []
-    authors_cse = []
-    authors_mhra = []
-    authors_mla = []
-    
-    def __init__(self, url_title):
-        self.page_url = mediawiki.page_data_url(settings.WIKIPROX_MEDIAWIKI_API, url_title)
-        self.cite_url = '%s?format=json&action=query&prop=info&prop=revisions&titles=%s' % (
-            settings.WIKIPROX_MEDIAWIKI_API, url_title)
-        self.url = self.cite_url
-        r = requests.get(self.cite_url)
-        self.status_code = r.status_code
-        pagedata = json.loads(r.text)
-        self.error = pagedata.get('error', None)
-        if (self.status_code == 200) and not self.error:
-            keys = pagedata['query']['pages'].keys()
-            if len(keys) == 1:
-                pageinfo = pagedata['query']['pages'][keys[0]]
-                self.title = pageinfo['title']
-                timestamp = pageinfo['revisions'][0]['timestamp']
-                self.lastmod = datetime.strptime(timestamp, mediawiki.TS_FORMAT)
-                self.retrieved = datetime.now()
-                self.uri = reverse('wikiprox-page', args=[url_title])
-                # get author info
-                r = requests.get(self.page_url)
-                pagedata2 = json.loads(r.text)
-                self.authors = mediawiki.find_author_info(pagedata2['parse']['text']['*'])
-                self.authors_apa = citations.format_authors_apa(self.authors['parsed'])
-                self.authors_bibtex = citations.format_authors_bibtex(self.authors['parsed'])
-                self.authors_chicago = citations.format_authors_chicago(self.authors['parsed'])
-                self.authors_cse = citations.format_authors_cse(self.authors['parsed'])
-                self.authors_mhra = citations.format_authors_mhra(self.authors['parsed'])
-                self.authors_mla = citations.format_authors_mla(self.authors['parsed'])
+    authors_apa = ''
+    authors_bibtex = ''
+    authors_chicago = ''
+    authors_cse = ''
+    authors_mhra = ''
+    authors_mla = ''
     
     def __repr__(self):
         return "<Citation '%s'>" % self.url_title
     
     def __str__(self):
         return self.url_title
-
-
-class SourceCitation(object):
-    encyclopedia_id = None
-    title = None
-    lastmod = None
-    retrieved = None
-    uri = None
     
-    def __init__(self, source):
-        source_dict = source.__dict__
-        self.encyclopedia_id = source.encyclopedia_id
-        self.title = self.encyclopedia_id
-        self.lastmod = source.modified
+    def __init__(self, page):
+        self.uri = page.uri
+        self.title = page.title
+        if getattr(page, 'lastmod', None):
+            self.lastmod = page.lastmod
+        elif getattr(page, 'modified', None):
+            self.lastmod = page.modified
         self.retrieved = datetime.now()
-        self.uri = reverse('wikiprox-source', args=[self.encyclopedia_id])
+        self.authors = page.authors
+        self.authors_apa = citations.format_authors_apa(self.authors['parsed'])
+        self.authors_bibtex = citations.format_authors_bibtex(self.authors['parsed'])
+        self.authors_chicago = citations.format_authors_chicago(self.authors['parsed'])
+        self.authors_cse = citations.format_authors_cse(self.authors['parsed'])
+        self.authors_mhra = citations.format_authors_mhra(self.authors['parsed'])
+        self.authors_mla = citations.format_authors_mla(self.authors['parsed'])
+
+
+class Proxy(object):
+    """Interface to back-end MediaWiki site and encyc-psms
     
-    def __repr__(self):
-        return "<SourceCitation '%s'>" % self.encyclopedia_id
+    NOTE: not a Django model object!
+    """
     
-    def __str__(self):
-        return self.encyclopedia_id
+    def articles_by_category(self):
+        articles_by_category = []
+        categories,titles_by_category = encyclopedia.articles_by_category()
+        for category in categories:
+            titles = [page['title'] for page in titles_by_category[category]]
+            articles_by_category.append( (category,titles) )
+        return articles_by_category
+    
+    def contents(self):
+        articles = [
+            {'first_letter':page['sortkey'][0].upper(), 'title':page['title']}
+            for page in encyclopedia.articles_a_z()
+        ]
+        return articles
+
+    def authors(self, columnize=True):
+        authors = [page['title'] for page in encyclopedia.published_authors()]
+        if columnize:
+            return _columnizer(authors, 4)
+        return authors
+
+    def page(self, url_title, request=None):
+        """
+        @param page: Page title from URL.
+        """
+        logger.debug(url_title)
+        page = Page()
+        page.url_title = url_title
+        page.uri = reverse('wikiprox-page', args=[url_title])
+        page.url = mediawiki.page_data_url(settings.WIKIPROX_MEDIAWIKI_API, page.url_title)
+        logger.debug(page.url)
+        auth = (settings.DANGO_HTPASSWD_USER, settings.DANGO_HTPASSWD_PWD)
+        r = requests.get(page.url, auth=auth)
+        page.status_code = r.status_code
+        logger.debug(page.status_code)
+        pagedata = json.loads(r.text)
+        page.error = pagedata.get('error', None)
+        if (page.status_code == 200) and not page.error:
+            page.public = False
+            ## hide unpublished pages on public systems
+            #page.public = request.META.get('HTTP_X_FORWARDED_FOR',False)
+            # note: header is added by Nginx, should not appear when connected directly
+            # to the app server.
+            page.published = mediawiki.page_is_published(pagedata)
+            page.lastmod = mediawiki.page_lastmod(settings.WIKIPROX_MEDIAWIKI_API, page.url_title)
+            # basic page context
+            page.title = pagedata['parse']['displaytitle']
+            page.title_sort = page.title
+            for prop in pagedata['parse']['properties']:
+                if prop.get('name',None) and prop['name'] and (prop['name'] == 'defaultsort'):
+                    page.title_sort = prop['*']
+            page.sources = mediawiki.find_primary_sources(
+                settings.TANSU_API,
+                pagedata['parse']['images'])
+            page.body = mediawiki.parse_mediawiki_text(
+                pagedata['parse']['text']['*'],
+                page.sources,
+                page.public,
+                False)
+            # rewrite media URLs on stage
+            # (external URLs not visible to Chrome on Android when connecting through SonicWall)
+            if hasattr(settings, 'STAGE') and settings.STAGE and request:
+                page.sources = sources.replace_source_urls(page.sources, request)
+            page.is_article = encyclopedia.is_article(page.title)
+            if page.is_article:
+                page.categories = [
+                    c['*'] for c in pagedata['parse']['categories']
+                    if not c.has_key('hidden')]
+                page.prev_page = encyclopedia.article_prev(page.title)
+                page.next_page = encyclopedia.article_next(page.title)
+                page.coordinates = mediawiki.find_databoxcamps_coordinates(pagedata['parse']['text']['*'])
+                page.authors = mediawiki.find_author_info(pagedata['parse']['text']['*'])
+            page.is_author = encyclopedia.is_author(page.title)
+            if page.is_author:
+                page.author_articles = encyclopedia.author_articles(page.title)
+        return page
+
+    def source(self, encyclopedia_id):
+        source = Source()
+        source.encyclopedia_id = encyclopedia_id
+        source.uri = reverse('wikiprox-source', args=[encyclopedia_id])
+        source.title = encyclopedia_id
+        data = sources.source(encyclopedia_id)
+        for key,val in data.iteritems():
+            setattr(source, key, val)
+        source.psms_id = int(data['id'])
+        source.original_size = int(data['original_size'])
+        source.created = datetime.strptime(data['created'], mediawiki.TS_FORMAT)
+        source.modified = datetime.strptime(data['modified'], mediawiki.TS_FORMAT)
+        if getattr(source, 'streaming_url', None) and ('rtmp' in source.streaming_url):
+            source.streaming_url = source.streaming_url.replace(settings.RTMP_STREAMER,'')
+            source.rtmp_streamer = settings.RTMP_STREAMER
+        return source
+
+    def citation(self, page):
+        return Citation(page)
--- front/wikiprox/views.py
+++ front/wikiprox/views.py
@@ -1,10 +1,5 @@
-from datetime import datetime, timedelta
 import json
-import os
-import re
 
-from bs4 import BeautifulSoup, SoupStrainer
-from bs4 import Comment
 import requests
 
 from django.conf import settings
@@ -14,9 +9,7 @@ from django.shortcuts import get_object_or_404, redirect, render_to_response
 from django.template import RequestContext
 from django.views.decorators.http import require_http_methods
 
-from wikiprox import mediawiki as mw
-from wikiprox import encyclopedia, sources, citations
-from wikiprox import models
+from wikiprox.models import Proxy as Backend
 
 
 @require_http_methods(['GET',])
@@ -27,18 +20,43 @@ def index(request, template_name='index.html'):
         context_instance=RequestContext(request)
     )
 
+def categories(request, template_name='wikiprox/categories.html'):
+    return render_to_response(
+        template_name,
+        {
+            'articles_by_category': Backend().articles_by_category(),
+        },
+        context_instance=RequestContext(request)
+    )
+
+def contents(request, template_name='wikiprox/contents.html'):
+    return render_to_response(
+        template_name,
+        {
+            'articles': Backend().contents(),
+        },
+        context_instance=RequestContext(request)
+    )
+
+def authors(request, template_name='wikiprox/authors.html'):
+    return render_to_response(
+        template_name,
+        {
+            'authors': Backend().authors(),
+        },
+        context_instance=RequestContext(request)
+    )
+
 @require_http_methods(['GET',])
 def page(request, url_title='index', printed=False, template_name='wikiprox/page.html'):
-    """
-    """
-    page = models.Page(url_title, printed=printed)
+    page = Backend().page(url_title)
     if page.error:
         raise Http404
     if (not page.published) and (not settings.WIKIPROX_SHOW_UNPUBLISHED):
         template_name = 'wikiprox/unpublished.html'
     elif page.is_author:
         template_name = 'wikiprox/author.html'
-    elif page.is_article and page.printed:
+    elif page.is_article and printed:
         template_name = 'wikiprox/article-print.html'
     else:
         template_name = 'wikiprox/article.html'
@@ -51,8 +69,28 @@ def page(request, url_title='index', printed=False, template_name='wikiprox/page
     )
 
 @require_http_methods(['GET',])
+def source(request, encyclopedia_id, template_name='wikiprox/source.html'):
+    source = Backend().source(encyclopedia_id)
+    if not source:
+        raise Http404
+    return render_to_response(
+        template_name,
+        {
+            'source': source,
+            'SOURCES_BASE': settings.SOURCES_BASE,
+            'rtmp_streamer': settings.RTMP_STREAMER,
+        },
+        context_instance=RequestContext(request)
+    )
+
+@require_http_methods(['GET',])
 def page_cite(request, url_title, template_name='wikiprox/cite.html'):
-    citation = models.Citation(url_title)
+    page = Backend().page(url_title)
+    if page.error or page.is_author:
+        raise Http404
+    if (not page.published) and (not settings.WIKIPROX_SHOW_UNPUBLISHED):
+        raise Http404
+    citation = Backend().citation(page)
     citation.href = 'http://%s%s' % (request.META['HTTP_HOST'], citation.uri)
     return render_to_response(
         template_name,
@@ -64,10 +102,10 @@ def page_cite(request, url_title, template_name='wikiprox/cite.html'):
 
 @require_http_methods(['GET',])
 def source_cite(request, encyclopedia_id, template_name='wikiprox/cite.html'):
-    source = models.Source(encyclopedia_id)
+    source = Backend().source(encyclopedia_id)
     if not source:
         raise Http404
-    citation = models.SourceCitation(source)
+    citation = Backend().citation(source)
     citation.href = 'http://%s%s' % (request.META['HTTP_HOST'], citation.uri)
     return render_to_response(
         template_name,
@@ -94,48 +132,6 @@ def media(request, filename, template_name='wikiprox/mediafile.html'):
         {
             'media_url': settings.TANSU_MEDIA_URL,
             'mediafile': mediafile,
-        },
-        context_instance=RequestContext(request)
-    )
-
-@require_http_methods(['GET',])
-def source(request, encyclopedia_id, template_name='wikiprox/source.html'):
-    source = models.Source(encyclopedia_id)
-    if not source:
-        raise Http404
-    return render_to_response(
-        template_name,
-        {
-            'source': source,
-            'SOURCES_BASE': settings.SOURCES_BASE,
-            'rtmp_streamer': settings.RTMP_STREAMER,
-        },
-        context_instance=RequestContext(request)
-    )
-
-def authors(request, template_name='wikiprox/authors.html'):
-    return render_to_response(
-        template_name,
-        {
-            'authors': models.Wiki.authors(),
-        },
-        context_instance=RequestContext(request)
-    )
-
-def categories(request, template_name='wikiprox/categories.html'):
-    return render_to_response(
-        template_name,
-        {
-            'articles_by_category': models.Wiki.articles_by_category(),
-        },
-        context_instance=RequestContext(request)
-    )
-
-def contents(request, template_name='wikiprox/contents.html'):
-    return render_to_response(
-        template_name,
-        {
-            'articles': models.Wiki.contents(),
         },
         context_instance=RequestContext(request)
     )

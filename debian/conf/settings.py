e6d6017bb7279a312dbb1addcaf02b027eba7b9e
geoffrey jost <geoffrey@jostwebwerks.com>
2017-04-04 15:55:04 -0700

    settings.py: get boolean values for debug flags

--- debian/conf/settings.py
+++ debian/conf/settings.py
@@ -27,9 +27,9 @@ if not configs_read:
 
 # ----------------------------------------------------------------------
 
-DEBUG = config.get('debug', 'debug')
+DEBUG = config.getboolean('debug', 'debug')
 TEMPLATE_DEBUG = DEBUG
-THUMBNAIL_DEBUG = config.get('debug', 'thumbnail')
+THUMBNAIL_DEBUG = config.getboolean('debug', 'thumbnail')
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts

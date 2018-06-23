3e7eec75289c5ad24c139e4c841c9266f1c6bf2d
geoffrey jost <geoffrey@jostwebwerks.com>
2018-06-22 18:03:43 -0700

    settings: added wikiprox.context_processors.sitewide
    How we get things like STATIC_URL into the context before?

--- conf/settings.py
+++ conf/settings.py
@@ -216,6 +216,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 #'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'wikiprox.context_processors.sitewide',
             ],
         },
     },

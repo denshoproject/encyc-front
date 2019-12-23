c17b12f967596d1c0ac75c96eb2e8b962112ecf1
Geoffrey Jost <geoffrey@jostwebwerks.com>
2019-12-23 14:24:35 -0800

    models: Import repo_models module

--- front/wikiprox/models/elastic.py
+++ front/wikiprox/models/elastic.py
@@ -17,6 +17,7 @@ from django.db import models
 from wikiprox import citations
 from wikiprox import ddr
 from wikiprox import docstore
+from wikiprox import repo_models
 from wikiprox import search
 from wikiprox import sources
 

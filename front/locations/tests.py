88c53a1bb9ca143dee35a11c73d8fc6aec98beeb
Geoffrey Jost <geoffrey@jostwebwerks.com>
2020-03-04 11:49:11 -0800

    tests: Use python3 b'' to compare requests.contents strings

--- front/locations/tests.py
+++ front/locations/tests.py
@@ -10,18 +10,18 @@ class APIView(TestCase):
     def test_locations(self):
         response = self.client.get(reverse('locations-api-locations'))
         assert response.status_code == 200
-        assert 'Temporary Assembly Center' in response.content
+        assert b'Temporary Assembly Center' in response.content
 
     def test_locationscategory(self):
         response = self.client.get(
             reverse('locations-api-category', args=['tac'])
         )
         assert response.status_code == 200
-        assert 'Temporary Assembly Center' in response.content
+        assert b'Temporary Assembly Center' in response.content
 
 
 class TimelineTests(TestCase):
     
     def test_firstdate(self):
         response = self.client.get(reverse('locations-index'))
-        assert 'Fresno, California' in response.content
+        assert b'Fresno, California' in response.content

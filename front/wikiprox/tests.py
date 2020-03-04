13f5c9da4af18c203bd134620caac7f2c8dccc20
Geoffrey Jost <geoffrey@jostwebwerks.com>
2020-03-04 11:50:26 -0800

    tests: Use reverse to get test URLs

--- front/wikiprox/tests.py
+++ front/wikiprox/tests.py
@@ -17,7 +17,7 @@ class APIView(TestCase):
 
     def test_article(self):
         assert self.client.get(
-            reverse('wikiprox-api-page', args=['Ansel%20Adams'])
+            reverse('wikiprox-api-page', args=['Ansel Adams'])
         ).status_code == 200
 
     def test_authors(self):
@@ -40,23 +40,29 @@ class WikiPageTitles(TestCase):
     
     def test_wiki_titles_space(self):
         assert self.client.get(
-            reverse('wikiprox-page', args=['Ansel%20Adams'])
+            reverse('wikiprox-page', args=['Ansel Adams'])
         ).status_code == 200
     
     #def test_wiki_titles_period(self):
     #    assert self.client.get('/A.L.%20Wirin/').status_code == 200
     
     def test_wiki_titles_hyphen(self):
-        assert self.client.get('/Aiko%20Herzig-Yoshinaga/').status_code == 200
+        assert self.client.get(
+            reverse('wikiprox-page', args=['Aiko Herzig-Yoshinaga'])
+        ).status_code == 200
     
     #def test_wiki_titles_parens(self):
     #    assert self.client.get('/Amache%20(Granada)/').status_code == 200
     
     def twiki_titleshars_comma(self):
-        assert self.client.get('/December%207,%201941December 7, 1941/').status_code == 200
+        assert self.client.get(
+            reverse('wikiprox-page', args=['December 7, 1941'])
+        ).status_code == 200
     
     #def test_wiki_titles_singlequote(self):
     #    assert self.client.get("/Hawai'i/").status_code == 200
     
     def test_wiki_titles_slash(self):
-        assert self.client.get('/Informants%20/%20"inu"/').status_code == 200
+        assert self.client.get(
+            reverse('wikiprox-page', args=['Informants / "inu"'])
+        ).status_code == 200
